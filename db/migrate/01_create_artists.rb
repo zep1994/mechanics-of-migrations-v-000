acc3b3bc67eee0cc49451e71119f38fad81b9240
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-01 21:58:35 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,10 @@
 class CreateArtists < ActiveRecord::Migration
-  def up
-  end
- 
-  def down
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
