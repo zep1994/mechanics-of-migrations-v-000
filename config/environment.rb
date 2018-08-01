ee50463d573cc01ec4d1927a458210311d87f7c0
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-08-01 22:03:28 +0000

    automatically backed up by learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,10 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
-
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 # put the code to connect to the database here
 
 
